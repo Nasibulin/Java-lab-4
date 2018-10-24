51b0f8ed5481e8a929f578ac7a5c34f211e254dc
Nasibulin <k.nasibulin@yandex.ru>
2018-10-24 07:43:43 +0300

    -

---
+++ src/test/java/StaffDemoTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 24.10.18
+ * Time: 8:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class StaffDemoTest {
+    public void testMain() throws Exception {
+
+    }
+}
